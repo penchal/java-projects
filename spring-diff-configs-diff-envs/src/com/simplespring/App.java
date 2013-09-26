c37740f53e7e0f0805b3a2ce0f74e5bc3faea738
seshumadhav <seshumadhav@gmail.com>
2013-09-26 11:33:38 +0530

    Passing production/test properties files at run time

--- spring-diff-configs-diff-envs/src/com/simplespring/App.java
+++ spring-diff-configs-diff-envs/src/com/simplespring/App.java
@@ -7,6 +7,9 @@ public class App {
 
   private MySampleBean mySampleBean;
 
+  // Run with -Denvironment=test to load test properties
+  // Run with -Denvironment=prod to load prod properties
+  // http://www.particlewave.com/2013/06/01/configuring-spring-for-test-versus-production/
   public App(MySampleBean mySampleBean) {
     this.mySampleBean = mySampleBean;
   }
