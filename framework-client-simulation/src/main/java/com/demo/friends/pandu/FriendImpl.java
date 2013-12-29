2a95ceda2478b9ed801f630223f66bc7cd0b568b
seshumadhav <seshumadhav@yahoo.com>
2013-12-29 11:38:26 +0530

    New Friend Impls

--- framework-client-simulation/src/main/java/com/demo/friends/pandu/FriendImpl.java
+++ framework-client-simulation/src/main/java/com/demo/friends/pandu/FriendImpl.java
@@ -1,16 +1,30 @@
-package com.demo.friends.friend1;
+package com.demo.friends.pandu;
 
 import java.util.Date;
 
 import com.demo.beans.Friend;
 
-public class Friend1 implements Friend {
-  
+public class FriendImpl implements Friend {
+
   private String tz = "GMT";
-  
+  private String myName;
+  private String specialMessage;
+
   @SuppressWarnings("deprecation")
   public String getTime() {
-    return tz + " asked time. I speak " + tz + ". Time now: " + (new Date()).toGMTString();
+    return myName + " asked time. I speak " + tz + ". Time now: "
+        + (new Date()).toGMTString() + ";;;;; Special Message: "
+        + specialMessage;
+  }
+
+  @Override
+  public void setName(String name) {
+    this.myName = name;
+  }
+
+  @Override
+  public void setSpecialMessage(String specialMessage) {
+    this.specialMessage = specialMessage;
   }
 
 }
--- framework-client-simulation/src/main/java/com/demo/friends/rakesh/FriendImpl.java
+++ framework-client-simulation/src/main/java/com/demo/friends/rakesh/FriendImpl.java
@@ -1,15 +1,29 @@
-package com.demo.friends.friend2;
+package com.demo.friends.rakesh;
 
 import java.util.Date;
 
 import com.demo.beans.Friend;
 
-public class Friend2 implements Friend {
-  
+public class FriendImpl implements Friend {
+
   private String tz = "LOCAL";
-  
+  private String myName;
+  private String specialMessage;
+
   public String getTime() {
-    return tz + " asked time. I speak " + tz + ". Time now: " + (new Date()).toLocaleString();
+    return myName + " asked time. I speak " + tz + ". Time now: "
+        + (new Date()).toString() + ";;;;; Special Message: "
+        + specialMessage;
+  }
+
+  @Override
+  public void setName(String name) {
+    this.myName = name;
+  }
+
+  @Override
+  public void setSpecialMessage(String specialMessage) {
+    this.specialMessage = specialMessage;
   }
 
 }
