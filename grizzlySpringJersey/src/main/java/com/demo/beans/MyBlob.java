e514076645a3f952a02c78333474072300e8b78a
seshumadhav <seshumadhav@yahoo.com>
2013-12-29 10:14:43 +0530

    JSON binding working now;

--- grizzlySpringJersey/src/main/java/com/demo/beans/MyBlob.java
+++ grizzlySpringJersey/src/main/java/com/demo/beans/MyBlob.java
@@ -5,6 +5,26 @@ import java.util.Date;
 public class MyBlob {
   
   private Date date;
+  
+  public Date getDate() {
+    return date;
+  }
+
+  
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  
+  public String getMessage() {
+    return message;
+  }
+
+  
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
   private String message;
   
   private MyBlob() {}
--- grizzlySpringJersey/src/main/java/com/demo/web/MyApplication.java
+++ grizzlySpringJersey/src/main/java/com/demo/web/MyApplication.java
@@ -18,20 +18,25 @@ public class MyApplication extends ResourceConfig {
   public MyApplication() {
     packages("com.demo.web");
     register(MoxyJsonFeature.class);
-    register(createMoxyJsonResolver());
-    registerInstances(new LoggingFilter(Logger.getLogger(MyApplication.class.getName()), true));
+    
+    // This was not required
+    // register(createMoxyJsonResolver());
+
+    LoggingFilter loggingFilter = new LoggingFilter(
+        Logger.getLogger(MyApplication.class.getName()), true);
+    registerInstances(loggingFilter);
   }
 
-  public static ContextResolver<MoxyJsonConfig> createMoxyJsonResolver() {
-    Map<String, String> namespacePrefixMapper = new HashMap<String, String>(1);
-    namespacePrefixMapper.put("http://www.w3.org/2001/XMLSchema-instance",
-        "xsi");
-    
-    final MoxyJsonConfig moxyJsonConfig = new MoxyJsonConfig()
-        .setNamespacePrefixMapper(namespacePrefixMapper)
-        .setNamespaceSeparator(':');
-    
-    return moxyJsonConfig.resolver();
-  }
+  //  public static ContextResolver<MoxyJsonConfig> createMoxyJsonResolver() {
+  //    Map<String, String> namespacePrefixMapper = new HashMap<String, String>(1);
+  //    namespacePrefixMapper.put("http://www.w3.org/2001/XMLSchema-instance",
+  //        "xsi");
+  //    
+  //    final MoxyJsonConfig moxyJsonConfig = new MoxyJsonConfig()
+  //        .setNamespacePrefixMapper(namespacePrefixMapper)
+  //        .setNamespaceSeparator(':');
+  //    
+  //    return moxyJsonConfig.resolver();
+  //  }
 
 }
